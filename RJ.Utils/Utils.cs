f120160ac5ad73cbbef8ff938807ab11d55dc896
rennishj <rennishj@gmail.com>
2016-02-05 21:26:01 -0500

    added utility project

---
+++ RJ.Utils/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJ.Utils
+{
+    public static  class Utils
+    {
+        public static void ParseCsvFile(string filePath)
+        { 
+            
+        }
+    }
+}
