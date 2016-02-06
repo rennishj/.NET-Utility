a042cbdb50cb8adf10da124664a0758ca34ef41c
rennishj <rennishj@gmail.com>
2016-02-05 21:13:35 -0500

    added sql scripts

---
+++ RJ.MVC/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using RJ.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RJ.MVC.Controllers
+{
+    public class LoginController : Controller
+    {        
+        public ActionResult Login()
+        {
+            LoginViewModel login = new LoginViewModel();
+            return View(login);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return Json(new { Id = 1, Uname = "rennishj@gmail.com", Password = "123456" });
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ RJ.MVC/Controllers/PersonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RJ.MVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return View();
+        }
+    }
+}
