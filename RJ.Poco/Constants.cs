8383dd7bd8a797a2d635777da8611d59c1cd7216
rennishj <rennishj@gmail.com>
2016-02-09 14:47:44 -0500

    added constants

---
+++ RJ.Poco/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJ.Poco
+{
+   public class Constants
+    {
+       public const float PI = 3.14f;
+
+    }
+}
