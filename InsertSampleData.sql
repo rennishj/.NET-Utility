578fd019b4f74a28972272b7be959c5ed2ed544e
rennishj <rennishj@gmail.com>
2016-02-12 19:55:32 -0500

    Added EF6

---
+++ InsertSampleData.sql
@@ -0,0 +1,41 @@
+﻿insert into dbo.MenuItemType
+(
+	Name
+)
+values
+(
+	'Link'
+)
+
+insert into dbo.MenuItem
+(
+	ParentId,
+	DisplayName,
+	Url,
+	MenuItemTypeId
+)
+values
+(
+	null,
+	'Home',
+	'http://www.asp.net',
+	1
+),
+(
+	1,
+	'Sql Helper',
+	'http://www.asp.net',
+	1
+),
+(
+	1,
+	'C# Helper',
+	'http://www.asp.net',
+	1
+),
+(
+	1,
+	'Xml Helper',
+	'http://www.asp.net',
+	1
+)
---
+++ RJ.DAL/DAL/MenuAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RJ.DAL.EF6;
+
+namespace RJ.DAL
+{
+   public class MenuAccess
+    {
+       public NetUtilityEntities2 Context
+       {
+           get { return new NetUtilityEntities2(); }
+       }
+       public List<MenuItem> GetAllMenuItems()
+       {
+           var menuITems =  Context.MenuItems.ToList();
+           return menuITems;
+       }
+    }
+}
