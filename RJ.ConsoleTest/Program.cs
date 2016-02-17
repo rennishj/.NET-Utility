38df2ede7b6f4cea99049ef9b584a312b47837ce
rennishj <rennishj@gmail.com>
2016-02-17 12:08:20 -0500

    Added Code for printing fibonacci numbers

--- RJ.ConsoleTest/Program.cs
+++ RJ.ConsoleTest/Program.cs
@@ -18,9 +18,16 @@ namespace RJ.ConsoleTest
                 //CsvHelper();
                 
                 //PalindromeTest();
-                TestRecusrion();
+                //TestRecusrion();
                 //TestSelectMany();
-                
+                Console.WriteLine("Please enter a number");
+                int number = Convert.ToInt32(Console.ReadLine());
+                var fib = PrintFibonacciRecursively(number);
+                foreach (var item in fib)
+                {
+                    Console.WriteLine(item);  
+                }
+                             
             }
             catch (Exception ex)
             {
@@ -34,6 +41,40 @@ namespace RJ.ConsoleTest
             }
             //Console.WriteLine("Finished wrting to the file");
             Console.ReadKey();
+
+        }
+
+        public static List<int> PrintFibonacci(int number)
+        {
+            List<int> result = new List<int>();
+            if (number == 0)
+            {
+                result.Add(0);
+                return result;
+            }
+            else if (number == 1)
+            {
+                result.Add(0);
+                return result;
+            }
+            else if (number == 2)
+            {
+                result.AddRange(new List<int>() { 0, 1 });
+                return result;
+            }
+            else
+            {
+                //if we got thus far,we should have f1,f2 and f3 as fibonacci numbers
+                int f1 = 0,
+                    f2 = 1;
+
+                result.AddRange(new List<int>() { f1, f2 });
+                for (int i = 2; i < number; i++)
+                {
+                    result.Add(result[i - 1] + result[i - 2]);
+                }
+            }
+            return result;
 
         }
 
