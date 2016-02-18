475d51ec996e0c0527abcc60646736187cdc6b58
rennishj <rennishj@gmail.com>
2016-02-18 16:10:13 -0500

    Added excel generator code

--- RJ.Poco/Attributes/ExcelAttributes.cs
+++ RJ.Poco/Attributes/ExcelAttributes.cs
@@ -9,21 +9,30 @@ namespace RJ.Poco.Attributes
     /// <summary>
     /// These are the attributes we should look out for while iterating through the object
     /// </summary>
-   public class ExcelHeader : Attribute
+   public class ExcelHeaderAttribute : Attribute
     {
         public string Name { get; set; }
-       public ExcelHeader(string headerName)
+        public ExcelHeaderAttribute(string headerName)
        {
            this.Name = headerName;
        }
     }
 
-   public class ExcelOrderAttribute : Attribute
+   public class ExcelColumnOrderAttribute : Attribute
    {
        public int ColumnOrder { get; set; }
-       public ExcelOrderAttribute(int columnOrder)
+       public ExcelColumnOrderAttribute(int columnOrder)
        {
            this.ColumnOrder = columnOrder;
+       }
+   }
+
+   public class ExcelFormatAttribute : Attribute
+   {
+       public string Format { get; set; }
+       public ExcelFormatAttribute(string format)
+       {
+           this.Format = format; 
        }
    }
 }
--- RJ.Utils/ExcelHelper.cs
+++ RJ.Utils/ExcelHelper.cs
@@ -7,6 +7,8 @@ using OfficeOpenXml;
 using OfficeOpenXml.Drawing;
 using OfficeOpenXml.Style;
 using System.IO;
+using RJ.Poco.Attributes;
+using System.Reflection;
 
 namespace RJ.Utils
 {
@@ -31,6 +33,82 @@ namespace RJ.Utils
             }
 
         }
+
+        public static ExcelPackage CreateExcel<T>(IEnumerable<T> data)
+        {
+            return null;
+        }
+
+        private static List<FieldInfo> BuildFieldInfo(Type tpe)
+        {
+            List<FieldInfo> retVal = new List<FieldInfo>();
+            var propertyInfos = tpe.GetProperties().Where(p => p.CanRead && p.GetCustomAttribute<ExcelHeaderAttribute>() != null).ToList();
+            int fiCounter = 0;
+            foreach (var pi in propertyInfos)
+            {
+                var displayOrderAttribute = pi.GetCustomAttribute<ExcelColumnOrderAttribute>();
+                var headerNameAttribute = pi.GetCustomAttribute<ExcelHeaderAttribute>();
+                var formatStringAttribute = pi.GetCustomAttribute<ExcelFormatAttribute>();
+                var typeCode  = Type.GetTypeCode(pi.PropertyType);
+                //To do : Check for Nullable Types
+                FieldInfo fi = new FieldInfo
+                {
+                    NaturalOrder = fiCounter++,
+                    DisplayOrder = Int32.MaxValue
+                };
+                switch (typeCode)
+                {                                    
+                    case TypeCode.Int16:                       
+                    case TypeCode.Int32:                       
+                    case TypeCode.Int64:
+                    case TypeCode.Decimal:
+                    case TypeCode.Double:
+                    case TypeCode.SByte:
+                    case TypeCode.UInt16:
+                    case TypeCode.UInt32:
+                    case TypeCode.UInt64:
+                    case TypeCode.Byte:
+                        fi.DataType = ExcelDataType.Number;
+                        break;
+                    case TypeCode.Boolean:
+                        fi.DataType = ExcelDataType.Boolean;
+                        break;
+                    case TypeCode.Char:
+                    case TypeCode.String:
+                        fi.DataType = ExcelDataType.String;
+                        break;
+                    case TypeCode.DateTime:
+                        fi.DataType = ExcelDataType.DateTime;
+                        break;   
+                    default:
+                        continue;//Object types has to be flattened to the primitive types
+                }
+                fi.DisplayOrder = displayOrderAttribute != null ? displayOrderAttribute.ColumnOrder : Int32.MaxValue;
+                fi.HeaderName = headerNameAttribute != null ? headerNameAttribute.Name : pi.Name;
+                fi.FormatString = formatStringAttribute != null ? formatStringAttribute.Format : null;
+                retVal.Add(fi);
+            }
+            return retVal.OrderBy(o => o.DisplayOrder).ThenBy(o => o.NaturalOrder).ToList();
+        }
+    }
+
+    public class FieldInfo
+    {
+        public int DisplayOrder { get; set; }
+        public string HeaderName { get; set; }
+        public string FormatString { get; set; }
+        public ExcelDataType DataType { get; set; }
+        public int NaturalOrder { get; set; }
+
+    }
+
+    public enum ExcelDataType
+    { 
+        Number = 1,
+        String,
+        DateTime,
+        Boolean
+
     }
 
 
