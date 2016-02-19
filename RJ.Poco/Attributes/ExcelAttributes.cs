063a8d7c2a7b1013028710bfa0ae851096053cd3
rennishj <rennishj@gmail.com>
2016-02-19 10:15:35 -0500

    changes

--- RJ.Poco/Attributes/ExcelAttributes.cs
+++ RJ.Poco/Attributes/ExcelAttributes.cs
@@ -9,39 +9,39 @@ namespace RJ.Poco.Attributes
     /// <summary>
     /// These are the attributes we should look out for while iterating through the object
     /// </summary>
-    [AttributeUsage(AttributeTargets.Property,AllowMultiple = false,Inherited = false)]
-   public class ExcelHeaderAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class ExcelHeaderAttribute : Attribute
     {
         public string Name { get; set; }
         public ExcelHeaderAttribute(string headerName)
-       {
-           this.Name = headerName;
-       }
+        {
+            this.Name = headerName;
+        }
     }
 
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-   public class ExcelColumnOrderAttribute : Attribute
-   {
-       public int ColumnOrder { get; set; }
-       public ExcelColumnOrderAttribute(int columnOrder)
-       {
-           this.ColumnOrder = columnOrder;
-       }
-   }
+    public class ExcelColumnOrderAttribute : Attribute
+    {
+        public int ColumnOrder { get; set; }
+        public ExcelColumnOrderAttribute(int columnOrder)
+        {
+            this.ColumnOrder = columnOrder;
+        }
+    }
 
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-   public class ExcelFormatAttribute : Attribute
-   {
-       public string Format { get; set; }
-       public ExcelFormatAttribute(string format)
-       {
-           this.Format = format; 
-       }
-   }
+    public class ExcelFormatAttribute : Attribute
+    {
+        public string Format { get; set; }
+        public ExcelFormatAttribute(string format)
+        {
+            this.Format = format;
+        }
+    }
 
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
     public class ExcelIgnoreAttribute : Attribute
     {
-        
+
     }
 }
